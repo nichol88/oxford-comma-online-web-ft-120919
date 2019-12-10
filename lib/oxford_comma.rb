29a4180804130bd3436758d9172826b2a0b69bff
Brian Nicholls <nichol88@gmail.com>
2019-12-10 00:16:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   if array.length > 1
     array.push("and #{array.pop}")
     array.join(", ")
+  elsif array.length == 2
+    array.join(" and ")
   else
     array[0]
   end
