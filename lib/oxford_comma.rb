e2274eb1471059fd5c3fd4d703e940e8761fce43
Brian Nicholls <nichol88@gmail.com>
2019-12-10 00:12:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length > 1
     pop = array.pop
     array.push("and")
-    array.push(pop)
+    array.push("and #{pop}")
     array.join(", ")
   else
     array[0]
