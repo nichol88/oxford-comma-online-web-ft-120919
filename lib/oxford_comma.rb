0276b5ee72624df7c92a167e312d2bfd5de129b0
Brian Nicholls <nichol88@gmail.com>
2019-12-10 00:16:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    array.push("and #{array.pop}")
+    array.join(", ")
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array[0]
+  end
 end
